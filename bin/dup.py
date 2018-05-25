313ae7a951ca3c658e93c072cd36fde8ece8821a
Vaibhav Gupta <vaibhav.gupta@gmail.com>
2018-05-25 05:32:54 +0530

    Fixing some of the syntax issue in dup.py

--- bin/dup.py
+++ bin/dup.py
@@ -1,30 +1,34 @@
 #!/usr/local/bin/python3
-
+"""
+Find duplicate code in the given file.
+"""
 import sys
 import logging
 import hashlib
 
 hash_funct = hashlib.md5
-rev_hash_map = {}
+REV_HASH_MAP = {}
 LINE_LENGTH = 5
 
-
 def read_file(file_name):
+    """read a given file"""
     with open(file_name, 'r') as file_handle:
         content = file_handle.read()
     return content
 
 def pre_process(content):
+    """Preprocess file"""
     out = [line.strip() for line in content if line is not ""]
     hash = [hash_funct(line.encode()).hexdigest() for line in out]
     hash_map  = {h:0 for h in set(hash)}
     for i, j in zip(hash, out):
         hash_map[i] = hash_map[i] + 1
-        rev_hash_map[i] = j
-    # print(rev_hash_map)
+        REV_HASH_MAP[i] = j
+    logging.error(REV_HASH_MAP)
     return (hash, hash_map)
 
 def process(data):
+    """process file"""
     (hash, hash_map) = data
     print("=" * 10 + hash[0])
     for i in range(0, len(hash)):
@@ -40,20 +44,21 @@ def process(data):
             if length > LINE_LENGTH:
                 print("-" * 40)
                 print(i, j, length)
-                for k in range(j,j+length):
-                    print(rev_hash_map[hash[k]])
+                for k in range(j, j+length):
+                    print(REV_HASH_MAP[hash[k]])
 
 def print_all(content_list):
+    """print content"""
     for i in content_list:
         for j in i:
             print(j)
 
 def find_dup_code(file_list):
+    """find duplicate code in a file"""
     content_list = [read_file(f).splitlines() for f in file_list]
     content_list = [pre_process(li) for li in content_list]
-    result = [process(li) for li in content_list]
+    _result = [process(li) for li in content_list]
     # print_all(content_list)
-
 
 if __name__ == "__main__":
     find_dup_code(sys.argv[1:])
