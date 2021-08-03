951bbf89d9a40b93eb9e946f84ad658893559103
Vaibhav Gupta <vaibhav.gupta@gmail.com>
2021-08-03 00:10:48 -0700

    adding init fish rc file

--- config.fish
+++ config.fish
@@ -1,5 +1,9 @@
 #/usr/local/bin/fish
 
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
 function ide
     tmux split-window -v -p 10
     tmux select-pane -t 1
--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 function link_file ()
 {
@@ -6,7 +6,7 @@ function link_file ()
     target="${HOME}/${1/dot/}"
 
     if [ -e "${target}" ]; then
-	mv $target $target.bak
+      mv $target $target.bak
     fi
 
     ln -sf ${source} ${target}
@@ -27,38 +27,41 @@ function check_file ()
 function help_file ()
 {
     echo "Usages: $0 [check|link|unlink|help]"
-    exit 0 
+    exit 0
+}
+
+function mlink_file()
+{
+    ln -sf ${PWD}/config.fish ~/.config/fish/config.fish
 }
 
 function main ()
 {
     command=${1-check}
 
-    # Check if the function exists or not. If not then exit.	
-    declare -F ${command}_file >/dev/null 
-    [ $? -ne 0 ] && echo "Command not found : $command" && help_file 
+    # Check if the function exists or not. If not then exit.
+    declare -F ${command}_file >/dev/null
+    [ $? -ne 0 ] && echo "Command not found : $command" && help_file
 
     # For each dot file
     for i in `find . -type f -name 'dot*' -printf "%f\n"`
     do
-	eval ${command}_file $i
-    done 
-
-    # For each dot directory. 
-    for d in `find . -type d -name 'dot*' -printf "%f\n"`
-    do 
-	mkdir ~/${d/dot/} 2> /dev/null > /dev/null
-    	for i in `find ${d} -maxdepth 1 -type f `
-	do
-		eval ${command}_file $i
-	done
-    	for i in `find ${d} -maxdepth 1 -mindepth 1 -type d`
-	do
-		eval ${command}_file $i
-	done
+      eval ${command}_file $i
     done
 
-
+    # For each dot directory.
+    for d in `find . -type d -name 'dot*' -printf "%f\n"`
+    do
+      mkdir ~/${d/dot/} 2> /dev/null > /dev/null
+      for i in `find ${d} -maxdepth 1 -type f `
+      do
+        eval ${command}_file $i
+      done
+      for i in `find ${d} -maxdepth 1 -mindepth 1 -type d`
+      do
+        eval ${command}_file $i
+      done
+    done
 }
 
 
