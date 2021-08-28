812d43d564a979d4c9f42b3a6ff918f277df6097
Vaibhav Gupta <vaibhav.gupta@gmail.com>
2021-08-27 23:06:29 -0700

    fix: updating the config.fish

--- config.fish
+++ config.fish
@@ -15,13 +15,30 @@ end
 # Default settings
 export EDITOR=vim
 
+# Default alias
+alias l="exa -l -snew -g --icons  --git-ignore --ignore-glob .git"
+alias lt="exa -l -snew -g --icons  --git-ignore --ignore-glob .git"
+alias g="git"
+
 # FZF Settings
 export FZF_DEFAULT_OPTS='--layout=reverse --border --layout=reverse --info=inline --preview "~/.vim/bundle/fzf.vim/bin/preview.sh {}"'
 export FZF_DEFAULT_COMMAND='fd --type f'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 fzf_configure_bindings --directory=\cp
 
+function rg
+  command rg -n $argv | fzf
+end
+
 # Install plugins
 function _install
+    fisher install jorgebucaran/fisher                                                                                                 │
     fisher install PatrickF1/fzf.fish
+    fisher install jethrokuan/z                                                                                                        │
+    fisher install simnalamburt/shellder                                                                                               │
+end
+
+# Start the theme
+function theme
+  starship init fish | source
 end
