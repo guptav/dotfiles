7dd5a35c9139e6b0637c476025886ef94c40234a
Vaibhav Gupta <vaibhav.gupta@gmail.com>
2023-02-21 00:24:32 -0800

    adding ctrl-o shortcut and fixing git cb command

--- config.fish
+++ config.fish
@@ -52,6 +52,7 @@ end
 
 # Keyboard Bindings
 bind \cs beginning-of-line
+bind \co "git cb"
 
 set PATH $PATH ~/bin
 # export DOCKER_HOST='unix:///Users/vaibhavgupta/.local/share/containers/podman/machine/podman-machine-default/podman.sock'
