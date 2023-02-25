18c7aba52833889ea989743a23e81331fbe692f3
Vaibhav Gupta <vaibhav.gupta@gmail.com>
2023-02-25 13:01:18 -0800

    syncing \ct \cg \co

--- config.fish
+++ config.fish
@@ -31,7 +31,7 @@ alias tkill="tmux list-sessions | awk '{print \$1}'| sed -e 's/$tmux_sess//g' | 
 export FZF_DEFAULT_OPTS='--layout=reverse --border --layout=reverse --info=inline --preview "~/.vim/bundle/fzf.vim/bin/preview.sh {}" --header "CTRL-O (open in browser) ╱ ALT-E (examine in editor)/ CTRL-/ (Change preview window)"  --bind "ctrl-/:change-preview-window(down,70%|hidden|)" --bind 'ctrl-w:toggle-preview-wrap'  --bind "ctrl-o:execute-silent:git op {}" --bind "alt-e:execute:vim {} > /dev/tty"'
 export FZF_DEFAULT_COMMAND='fd --type f'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-fzf_configure_bindings --directory=\cp --git_log=\cg
+fzf_configure_bindings --directory=\cp
 
 function rg
   command rg -n $argv | fzf
@@ -50,11 +50,6 @@ function theme
   starship init fish | source
 end
 
-# Keyboard Bindings
-bind \cs beginning-of-line
-bind \co "git cb"
-bind \ct "git fuzzy status"
-
 set PATH $PATH ~/bin
 # export DOCKER_HOST='unix:///Users/vaibhavgupta/.local/share/containers/podman/machine/podman-machine-default/podman.sock'
 
@@ -63,3 +58,9 @@ export GIT_FUZZY_STATUS_ADD_KEY='Ctrl-g'
 export GIT_FUZZY_STATUS_RESET_KEY='Ctrl-f'
 export GF_DIFF_FILE_PREVIEW_DEFAULTS="--color"
 set -x PATH "/Users/vaibhavgupta/repo/git-fuzzy/bin:$PATH"
+
+# Keyboard Bindings
+bind \cs beginning-of-line
+bind \co "git cb"
+bind \ct "git fuzzy status"
+bind \cg "git gl"
