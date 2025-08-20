70f3a6543f0930464e54221f8c1ef677ffc9629e
Vaibhav Gupta <vaibhavgupta@box.com>
2025-08-20 00:12:33 -0700

    fix: adding the init-mac file

--- init-mac.sh
+++ init-mac.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-brew install tmux git nvim gh fisher make unzip gcc 
-brew install fish fzf ripgrep fisher fd python3 task vim tpm gh awscli jfrog-cli
+# Basic Tools
+brew install tmux git vim nvim fish fisher make unzip gcc bat
+brew install fzf ripgrep fd python3 task tpm
+brew install gh awscli jfrog-cli
 brew install gopls markdownlint-cli
+
+brew install htop
